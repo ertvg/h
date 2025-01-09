05f1ed2bae418d04134622fca3f3ce9ee68a1df2
ertvg <simjkl@hotmail.fr>
2025-01-09 18:52:31 +0100

    Update ph.ps1

--- ph.ps1
+++ ph.ps1
@@ -1,15 +1,25 @@
-# Monter le volume au maximum
-[System.Media.SystemSounds.Beep].Play() # Simule un bip sonore pour s'assurer que le son fonctionne
-$null = New-Object -ComObject "WMPlayer.OCX"
-$player.settings.volume = 100
+# Monter le volume au maximum (approche plus robuste)
+[System.Media.SystemSounds.Beep].Play()
+$audio = New-Object -ComObject "WMPlayer.OCX"
+$audio.settings.volume = 100
 
 # Ouvrir une page internet spécifique et la mettre en plein écran
-$url = "https://fr.pornhub.com/view_video.php?viewkey=676a98e029f49" # Remplacez par l'URL souhaitée
+$url = "https://www.exemple.com"
 Start-Process -FilePath "chrome.exe" -ArgumentList $url
 
-# Mettre la fenêtre en plein écran (Cette partie peut varier selon le navigateur)
-# Pour Chrome, vous pouvez utiliser un raccourci clavier via AutoIt
+# Mettre la fenêtre en plein écran (utilisation d'AutoIt avec vérification du titre)
 Add-Type -AssemblyName AutoItX3
 $autoit = New-Object -ComObject AutoItX3.AutoItX3
-$autoit.WinActivate("Chrome")
-$autoit.Send("{F11}") # Appuie sur F11 pour passer en plein écran
+
+# Attendre que Chrome se charge complètement
+Start-Sleep -Seconds 2
+
+# Trouver la fenêtre de Chrome et la mettre en plein écran
+$title = "Chrome"  # Remplacez par le titre exact de votre fenêtre Chrome
+$hwnd = $autoit.WinWaitActive($title)
+if ($hwnd) {
+    $autoit.WinActivate($hwnd)
+    $autoit.Send("{F11}")
+} else {
+    Write-Warning "Impossible de trouver la fenêtre Chrome."
+}
