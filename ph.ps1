d30898ef0a181869e5b231ade30afd268d327e11
ertvg <simjkl@hotmail.fr>
2025-01-09 18:58:32 +0100

    Update ph.ps1

--- ph.ps1
+++ ph.ps1
@@ -1,17 +1,3 @@
-# This turns the volume up to max level
-$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
-
-# Ouvrir une page internet spécifique et la mettre en plein écran
-$url = "https://www.exemple.com"
-Start-Process -FilePath "chrome.exe" -ArgumentList $url
-
-# Mettre la fenêtre en plein écran (utilisation d'AutoIt avec vérification du titre)
-Add-Type -AssemblyName AutoItX3
-$autoit = New-Object -ComObject AutoItX3.AutoItX3
-
-# Attendre que Chrome se charge complètement
-Start-Sleep -Seconds 2
-
 function Pause-Script{
 Add-Type -AssemblyName System.Windows.Forms
 $originalPOS = [System.Windows.Forms.Cursor]::Position.X
@@ -27,5 +13,20 @@ $o=New-Object -ComObject WScript.Shell
         }
     }
 }
+# This turns the volume up to max level
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+# Ouvrir une page internet spécifique et la mettre en plein écran
+$url = "https://fr.pornhub.com/view_video.php?viewkey=676a98e029f49"
+Start-Process -FilePath "chrome.exe" -ArgumentList $url
+
+Add-Type -AssemblyName AutoItX3
+
+$autoit = New-Object -ComObject AutoItX3.AutoItX3
+$autoit.Send("{F11}")
+
+# Attendre que Chrome se charge complètement
+Start-Sleep -Seconds 2
+
 
 
