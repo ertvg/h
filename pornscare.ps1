1263f095ad0a6f9f4098544b84f6e23c9330dcfb
ertvg <simjkl@hotmail.fr>
2025-01-09 16:37:21 +0100

    Update pornscare.ps1

--- pornscare.ps1
+++ pornscare.ps1
@@ -31,7 +31,7 @@
 
 # Download Image; replace link to $image to add your own image
 
-$image =  "https://github.com/ertvg/h/blob/main/scream.png"
+$image =  "$image"
 
 $i = -join($image,"?dl=1")
 iwr $i -O $env:TMP\i.png
