1574fd03c5f0c70f09cc20bc21ef0cb4d4f2d012
ertvg <simjkl@hotmail.fr>
2025-01-10 23:51:29 +0100

    Update r.ps1

--- r.ps1
+++ r.ps1
@@ -4,13 +4,22 @@ function AfficherBoiteDialogue {
     $message = "Voulez-vous continuer ?"
     $titre = "Confirmation"
     $boutons = [System.Windows.Forms.MessageBoxButtons]::YesNo
+
+    # Afficher la boîte de dialogue et stocker le résultat
     $resultat = [System.Windows.Forms.MessageBox]::Show($message, $titre, $boutons)
 
-    if ($resultat -eq [System.Windows.Forms.DialogResult]::Yes) {
-        Write-Host "Vous avez cliqué sur Oui"
-    } else {
-        Write-Host "Vous avez cliqué sur Non"
+    # Actions en fonction du résultat
+    switch ($resultat) {
+        [System.Windows.Forms.DialogResult]::Yes {
+            Write-Host "Vous avez cliqué sur Oui"
+            # Ajoutez ici les actions à effectuer si l'utilisateur clique sur Oui
+        }
+        [System.Windows.Forms.DialogResult]::No {
+            Write-Host "Vous avez cliqué sur Non"
+            # Ajoutez ici les actions à effectuer si l'utilisateur clique sur Non
+        }
     }
 }
 
+# Appeler la fonction pour afficher la boîte de dialogue
 AfficherBoiteDialogue
