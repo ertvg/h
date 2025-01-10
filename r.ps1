91c2040eba5e7b6dd3047ee2c6c7f0ead3ae0835
ertvg <simjkl@hotmail.fr>
2025-01-10 23:24:24 +0100

    Update r.ps1

--- r.ps1
+++ r.ps1
@@ -1,21 +1,11 @@
 # Spécifie l'URL du fichier audio sur GitHub
-$url = "https://github.com/ertvg/h/blob/main/ALRM.wav"
+$url = "https://github.com/ertvg/h/blob/main/sf_pet_12.mp3"
 
 # Spécifie le chemin de destination du fichier téléchargé
-$destination = "C:\Temp\ALRM.wav"
+$destination = "C:\Temp\sf_pet_12.mp3"
 
-try {
-    # Télécharge le fichier
-    Invoke-WebRequest -Uri $url -OutFile $destination
+# Télécharge le fichier
+Invoke-WebRequest -Uri $url -OutFile $destination
 
-    # Vérifie si le fichier a été téléchargé
-    if (Test-Path $destination) {
-        Write-Host "Le fichier a été téléchargé avec succès."
-        # Ouvre le fichier avec le lecteur multimédia par défaut
-        Start-Process $destination
-    } else {
-        Write-Host "Erreur lors du téléchargement du fichier."
-    }
-} catch {
-    Write-Host "Une erreur s'est produite :" $_.Exception.Message
-}
+# Ouvre le fichier avec le lecteur multimédia par défaut
+Start-Process $destination
