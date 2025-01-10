eabcaff4b26b92ce945edb15d0c81ef761fd7895
ertvg <simjkl@hotmail.fr>
2025-01-10 23:57:04 +0100

    Update r.ps1

--- r.ps1
+++ r.ps1
@@ -1,20 +1,20 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
-function AfficherBoiteDialogue($x, $y, $largeur, $hauteur, $titre = "Boîte de dialogue") {
-    $form = New-Object System.Windows.Forms.Form
-    $form.Text = $titre
-    $form.Size = New-Object System.Drawing.Size($largeur, $hauteur)
-    $form.StartPosition = "Manual"
-    $form.Location = New-Object System.Drawing.Point($x, $y)
-    $form.ShowDialog()
+function AfficherBoiteDialogue {
+    $message = "Es-tu là ?"
+    $titre = "Confirmation"
+    $boutons = [System.Windows.Forms.MessageBoxButtons]::YesNo
+
+    $resultat = [System.Windows.Forms.MessageBox]::Show($message, $titre, $boutons)
+
+    switch ($resultat) {
+        [System.Windows.Forms.DialogResult]::Yes {
+            Write-Host "Vous avez répondu Oui"
+        }
+        [System.Windows.Forms.DialogResult]::No {
+            Write-Host "Vous avez répondu Non"
+        }
+    }
 }
 
-# Obtenir la résolution de l'écran principal
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$screenWidth = $screen.Bounds.Width
-$screenHeight = $screen.Bounds.Height
-
-# Afficher les boîtes de dialogue à différentes positions
-AfficherBoiteDialogue (($screenWidth / 2) - 150, ($screenHeight / 2) - 50, 300, 100, "Boîte au centre")
-AfficherBoiteDialogue 0, 0, 200, 100, "Boîte en haut à gauche"
-AfficherBoiteDialogue ($screenWidth - 200), $screenHeight - 100, 250, 150, "Boîte en bas à droite"
+AfficherBoiteDialogue
